9b397c7e1295c6ab9a44ded57c353c226a6dfdca
MarcosX <79601596+Marco5X@users.noreply.github.com>
2022-10-11 21:33:53 -0300

    Avance ejercicio 26

---
+++ ejercicio-react/src/components/Ejercicio 25-26/AppContext.js
@@ -0,0 +1,13 @@
+import { TaskComponent } from './components/TaskComponent';
+
+
+function AppContext() {
+
+  return (
+    <div >
+        <TaskComponent />
+    </div>
+  );
+}
+
+export default AppContext;
--- ejercicio-react/src/index.js
+++ ejercicio-react/src/index.js
@@ -8,17 +8,28 @@ import { Ruteo } from './components/Ejercicio 16-18/Ruteo';
 import AppRedux from "./components/Ejercicio 22-24/AppRedux"
 import { store } from "./components/Ejercicio 22-24/redux/config/storeConfig"
 import { createAppStore } from "./components/Ejercicio 22-24/redux/config/store.js"
+import { TaskComponent } from './components/Ejercicio 25-26/components/TaskComponent';
 
-let appStore = createAppStore()
+// let appStore = createAppStore()
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <Provider store={appStore}>
+//     <React.StrictMode>
+//       {/* <App /> */}
+//       {/* <Ruteo /> */}
+//       <AppRedux />
+//     </React.StrictMode>
+//   </Provider>
+// );
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={appStore}>
     <React.StrictMode>
       {/* <App /> */}
       {/* <Ruteo /> */}
-      <AppRedux />
+      <TaskComponent />
     </React.StrictMode>
-  </Provider>
 );
 
 reportWebVitals();
